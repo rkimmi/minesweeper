b42d13b47e35b7e47510637744407df4344b833b
Kimmi Rindel <kimmi@Kimmis-MBP.hub>
2017-12-16 10:25:58 +1300

    counts are working

--- minesweeper.js
+++ minesweeper.js
@@ -2,22 +2,23 @@ document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
 var board = {
-cells: [ { row: 0, col: 0, isMine: true, hidden: true},
-{ row: 0, col: 1, isMine: true, hidden: true},
-{ row: 0, col: 2, isMine: true, hidden: true},
-{ row: 1, col: 0, isMine: true, hidden: true},
-{ row: 1, col: 1, isMine: true, hidden: true},
-{ row: 1, col: 2, isMine: true, hidden: true},
-{ row: 2, col: 0, isMine: true, hidden: true},
-{ row: 2, col: 1, isMine: true, hidden: true},
-{ row: 2, col: 2, isMine: true, hidden: true},
-
+  cells: [
+  {row: 0, col: 0, isMine: true, hidden: true},
+  {row: 0, col: 1, isMine: true, hidden: true},
+  {row: 0, col: 2, isMine: true, hidden: true},
+  {row: 1, col: 0, isMine: true, hidden: true},
+  {row: 1, col: 1, isMine: false, hidden: true},
+  {row: 1, col: 2, isMine: false, hidden: true},
+  {row: 2, col: 0, isMine: false, hidden: true},
+  {row: 2, col: 1, isMine: false, hidden: true},
+  {row: 2, col: 2, isMine: false, hidden: true},
 ]
-
-}
-
+};
 
 function startGame () {
+  for (var i = 0; i < board.cells.length; i++) {
+  board.cells[i]["surroundingMines"] = countSurroundingMines(board.cells[i])
+}
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
@@ -37,9 +38,17 @@ function checkForWin () {
 // (there could be as many as 8). You don't have to get the surrounding
 // cells yourself! Just use `lib.getSurroundingCells`:
 //
-//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
 //
 // It will return cell objects in an array. You should loop through
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
+  var count = 0;
+  var surrounding = lib.getSurroundingCells(cell.row, cell.col)
+
+for (var i = 0; i < surrounding.length; i++) {
+  if (surrounding[i].isMine) {
+    count++;
+  }
+}
+return count;
 }
